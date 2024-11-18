6b226970e03f2cbe89cbbb8d8363ba3fada912f7
flyaka <flyaka1991@mail.ru>
2024-11-18 17:51:15 +0300

    uncomment wheater

--- BackEnd/Controllers/WeatherForecastController.cs
+++ BackEnd/Controllers/WeatherForecastController.cs
@@ -1,4 +1,4 @@
-/*using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BackEnd.Controllers
 {
@@ -30,4 +30,4 @@ namespace BackEnd.Controllers
             .ToArray();
         }
     }
-/*}
+}
