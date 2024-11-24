9e599046e32110e76676215d9fb84c6958b22bd6
flyaka <flyaka1991@mail.ru>
2024-11-24 23:36:01 +0300

    CreateRegisterUserRequest

---
+++ BackEnd/Models/Requests/Validators/RegisterUserRequestValidator.cs
@@ -0,0 +1,13 @@
+﻿using BackEnd.Models.Requests.UserRequests;
+using FluentValidation;
+
+namespace BackEnd.Models.REQUESTS.Validators;
+
+public class RegisterUserRequestValidator : AbstractValidator<RegisterUserRequest>
+{
+    public RegisterUserRequestValidator()
+    {
+        RuleFor(model => model.Email).EmailAddress();
+        RuleFor(model => model.Password).MinimumLength(8);
+    }   
+}
