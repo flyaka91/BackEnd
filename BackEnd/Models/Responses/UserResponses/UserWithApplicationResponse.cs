d30772e53ca3d0b83973bdd901b5ed9003592402
flyaka <flyaka1991@mail.ru>
2024-11-24 23:37:50 +0300

    CreateUserWithApplicationResponse

---
+++ BackEnd/Models/Responses/UserResponses/UserWithApplicationResponse.cs
@@ -0,0 +1,10 @@
+﻿using BackEnd.Models.Responses.BookResponses;
+using BackEnd.Models.Responses.ApplicationResponses;
+
+namespace BackEnd.Models.Responses.UserResponses;
+
+public class UserWithApplicationResponse : UserResponse
+{
+    public DateTime ApplicationDate { get; set; }
+    public List<ApplicationResponse> Applications { get; set; }
+}
