883833a715feb15be6ac7571131316d38dee449d
flyaka <flyaka1991@mail.ru>
2024-11-24 23:21:27 +0300

    CreateApplicationController

---
+++ BackEnd/Controllers/ApplicationController.cs
@@ -0,0 +1,28 @@
+﻿using BackEnd.Models.Requests.ApplicationRequests;
+using BackEnd.Models.Requests.UserRequests;
+using BackEnd.Models.Responses.ApplicationResponses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackEnd.Controllers;
+
+[ApiController]
+[Route("api/applications")]
+public class ApplicationController : ControllerBase
+{
+    
+    [HttpPost]
+    public ActionResult<Guid> Create([FromBody] CreateApplicationRequest request)
+    
+    {
+        var CreatedApplicationId = Guid.NewGuid();
+        return Ok(CreatedApplicationId);
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteApplication([FromRoute] Guid id)
+    {
+        return NoContent();
+    }
+}
+
+
